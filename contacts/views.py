1472704cad847d1046969f222e2948bc9e90fb8b
ThatcherK <kaliithatmau@gmail.com>
2020-11-23 15:58:58 +0300

    initial commit

---
+++ contacts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Contact
+from .serializers import ContactSerialzer
+from rest_framework import permissions
+
+class ContactList(ListCreateAPIView):
+    serializer_class= ContactSerialzer
+    permission_classes=(permissions.IsAuthenticated,)
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return Contact.objects.filter(owner=self.request.user)
+
+class ContactDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class= ContactSerialzer
+    permission_classes=(permissions.IsAuthenticated,)
+    lookup_field = "id"
+    def get_queryset(self):
+        return Contact.objects.filter(owner=self.request.user)
